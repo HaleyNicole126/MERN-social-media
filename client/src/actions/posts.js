ccdba3376f5382f079a366825fa82bcef7cbb8b6
haybales <haleynicole126@gmail.com>
2022-11-16 20:55:53 -0700

    posts with material icons

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -11,7 +11,6 @@ export const getPosts = () => async (dispatch) => {
     }
     
 }
-
 export const createPost = (post) => async(dispatch) => {
     try {
         const { data } = await api.createPost(post);
